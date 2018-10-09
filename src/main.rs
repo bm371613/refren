9dd9fc0d65b9f75368d4a43f9cb59e8721b123d4
Bartosz Marcinkowski <bm371613@gmail.com>
2018-10-09 22:59:51 +0200

    introduced clap for a better CLI experience

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
+extern crate clap;
+
 mod name;
 mod replace;
 mod style;
 
 use std::io;
 
+use clap::{App, Arg};
+
 use name::Name;
 use replace::default_replacer;
 use style::NamingStyle;
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = std::env::args().skip(1).take(2).collect();
-    let old_name = Name::parse(&args[0]).unwrap();
-    let new_name = Name::parse(&args[1]).unwrap();
+    let matches = App::new("rr: refactor/rename")
+        .version("0.0")
+        .author("Bartosz Marcinkowski <bm371613@gmail.com>")
+        .about("Facilitates refactoring/renaming")
+        .arg(
+            Arg::with_name("OLD_NAME")
+                .help("Old name")
+                .required(true)
+                .index(1),
+        ).arg(
+            Arg::with_name("NEW_NAME")
+                .help("New name")
+                .required(true)
+                .index(2),
+        ).get_matches();
+
+    let old_name = Name::parse(matches.value_of("OLD_NAME").unwrap()).unwrap();
+    let new_name = Name::parse(matches.value_of("NEW_NAME").unwrap()).unwrap();
     let replacer = default_replacer(
         style::STYLES
             .iter()
