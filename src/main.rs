30d60e036b4f086e6796a6394e2b7b0978a3a390
Bartosz Marcinkowski <bm371613@gmail.com>
2018-10-15 23:45:58 +0200

    in place, in memory

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,18 @@ mod name;
 mod style;
 mod transform;
 
+use std::fs::File;
 use std::io;
-use std::process;
+use std::io::Read;
 
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 use name::Name;
 use style::NamingStyle;
 use transform::default_transformer;
 
-fn print_error_and_exit(message: &str) -> ! {
-    eprintln!("{}", message);
-    process::exit(1);
-}
-
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+    let app = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -33,21 +29,44 @@ fn main() {
                 .help("New name")
                 .required(true)
                 .index(2),
-        ).get_matches();
+        ).arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("Modify the given file instead of using standard input and output")
+                .takes_value(true),
+        ).after_help(
+            "\
+             Both names should be space-separated lists of tokens. \n\
+             Use uppercase for acronyms, lowercase otherwise, eg. \"bad HTTP response\"",
+        );
 
-    let old_name = Name::parse(matches.value_of("OLD_NAME").unwrap())
-        .unwrap_or_else(|e| print_error_and_exit(&format!("OLD_NAME parsing error: {}", e)));
-    let new_name = Name::parse(matches.value_of("NEW_NAME").unwrap())
-        .unwrap_or_else(|e| print_error_and_exit(&format!("NEW_NAME parsing error: {}", e)));
+    run_command(app.get_matches()).unwrap_or_else(|e| {
+        eprintln!("ERROR: {}", e);
+        std::process::exit(1);
+    });
+}
+
+fn run_command(arg_matches: ArgMatches) -> Result<(), Box<std::error::Error>> {
+    let old_name = Name::parse(arg_matches.value_of("OLD_NAME").unwrap())?;
+    let new_name = Name::parse(arg_matches.value_of("NEW_NAME").unwrap())?;
     if old_name.is_empty() {
-        print_error_and_exit("OLD_NAME is empty")
+        return Err("OLD_NAME is empty".into());
     }
     let transformer = default_transformer(
         style::STYLES
             .iter()
             .map(|s| (s.format(old_name.singular()), s.format(new_name.singular()))),
     );
-    transformer
-        .transform(&mut io::stdin(), &mut io::stdout())
-        .unwrap_or_else(|e| print_error_and_exit(&e.to_string()));
+    match arg_matches.value_of("file") {
+        Some(file_name) => {
+            // TODO optionally use a temporary file instead of memory
+            let mut content: Vec<u8> = Vec::new();
+            File::open(file_name)?.read_to_end(&mut content)?;
+            transformer.transform(&mut &content[..], &mut File::create(file_name)?)
+        }
+        None => transformer.transform(&mut io::stdin(), &mut io::stdout()),
+    }?;
+    Ok(())
 }
