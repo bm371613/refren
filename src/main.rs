8728756bc25388bc256ade272f98f32fd57a6211
Bartosz Marcinkowski <bm371613@gmail.com>
2018-10-15 15:22:37 +0200

    CLI metadata from cargo

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ fn print_error_and_exit(message: &str) -> ! {
 }
 
 fn main() {
-    let matches = App::new("rr: refactor/rename")
-        .version("0.0")
-        .author("Bartosz Marcinkowski <bm371613@gmail.com>")
-        .about("Facilitates refactoring/renaming")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("OLD_NAME")
                 .help("Old name")
