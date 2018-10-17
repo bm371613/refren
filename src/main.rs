e5d46b90ad1d25e4cec9daca9c5628a65c8ece8e
Bartosz Marcinkowski <bm371613@gmail.com>
2018-10-17 22:37:33 +0200

    main split into 3 functions

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,46 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
-use clap::{App, Arg, ArgMatches};
+use clap::{App, Arg};
 
 use name::Name;
 use style::NamingStyle;
 use transform::default_transformer;
 
 fn main() {
-    let app = App::new(env!("CARGO_PKG_NAME"))
+    run_command(std::env::args_os()).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    });
+}
+
+fn run_command(args_os: std::env::ArgsOs) -> Result<(), Box<std::error::Error>> {
+    let arg_matches = build_app().get_matches_from_safe(args_os)?;
+
+    let old_name = Name::parse(arg_matches.value_of("OLD_NAME").unwrap())?;
+    let new_name = Name::parse(arg_matches.value_of("NEW_NAME").unwrap())?;
+    if old_name.is_empty() {
+        return Err("OLD_NAME is empty".into());
+    }
+    let transformer = default_transformer(
+        style::STYLES
+            .iter()
+            .map(|s| (s.format(old_name.singular()), s.format(new_name.singular()))),
+    );
+    match arg_matches.value_of("file") {
+        Some(file_name) => {
+            // TODO optionally use a temporary file instead of memory
+            let mut content: Vec<u8> = Vec::new();
+            File::open(file_name)?.read_to_end(&mut content)?;
+            transformer.transform(&mut &content[..], &mut File::create(file_name)?)
+        }
+        None => transformer.transform(&mut io::stdin(), &mut io::stdout()),
+    }?;
+    Ok(())
+}
+
+fn build_app<'a, 'b>() -> App<'a, 'b> {
+    App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -40,33 +72,5 @@ fn main() {
             "\
              Both names should be space-separated lists of tokens. \n\
              Use uppercase for acronyms, lowercase otherwise, eg. \"bad HTTP response\"",
-        );
-
-    run_command(app.get_matches()).unwrap_or_else(|e| {
-        eprintln!("ERROR: {}", e);
-        std::process::exit(1);
-    });
-}
-
-fn run_command(arg_matches: ArgMatches) -> Result<(), Box<std::error::Error>> {
-    let old_name = Name::parse(arg_matches.value_of("OLD_NAME").unwrap())?;
-    let new_name = Name::parse(arg_matches.value_of("NEW_NAME").unwrap())?;
-    if old_name.is_empty() {
-        return Err("OLD_NAME is empty".into());
-    }
-    let transformer = default_transformer(
-        style::STYLES
-            .iter()
-            .map(|s| (s.format(old_name.singular()), s.format(new_name.singular()))),
-    );
-    match arg_matches.value_of("file") {
-        Some(file_name) => {
-            // TODO optionally use a temporary file instead of memory
-            let mut content: Vec<u8> = Vec::new();
-            File::open(file_name)?.read_to_end(&mut content)?;
-            transformer.transform(&mut &content[..], &mut File::create(file_name)?)
-        }
-        None => transformer.transform(&mut io::stdin(), &mut io::stdout()),
-    }?;
-    Ok(())
+        )
 }
